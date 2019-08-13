eb7b94ac56c1853e93bd6c3d5a90c73ecf905c0b
fionastefanilimin <fiona16ti@mahasiswa.pcr.ac.id>
2019-08-13 18:51:50 +0700

    make directive

--- todos-app/resources/views/todos/index.blade.php
+++ todos-app/resources/views/todos/index.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
- 
-    <div class="container">
-        <h1 class="text-center my-5">Todos App</h1>
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-center my-5">Todos App</h1>
         <div class="row justify-content-center">
             <div class="col-md-8">
                     <div class="card card-default">
@@ -32,6 +22,5 @@
                 </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+    
+@endsection
